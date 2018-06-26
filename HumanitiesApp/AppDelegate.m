da1f43e28245c124635c156d041e30a5e973160d
Mike Dallow <mikedallow@earthlink.net>
2018-06-25 18:42:14 -0700

    Print statement for testing

--- HumanitiesApp/AppDelegate.m
+++ HumanitiesApp/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    printf("Hello\n");
     return YES;
 }
 
