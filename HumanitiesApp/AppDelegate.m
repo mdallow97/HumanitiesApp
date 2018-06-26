8a1245e868961340fd8a69bdf22168ea1054fe76
Mike Dallow <mikedallow@earthlink.net>
2018-06-25 18:43:40 -0700

    Deleted print statement

--- HumanitiesApp/AppDelegate.m
+++ HumanitiesApp/AppDelegate.m
@@ -17,7 +17,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    printf("Hello\n");
     return YES;
 }
 
